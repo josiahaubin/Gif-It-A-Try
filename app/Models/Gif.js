2182f160e172c8e3162e0a4261917ca9545157c6
Josiah Aubin <josiahaubin@gmail.com>
2019-08-28 13:56:34 -0600

    Trending gifs template

--- app/Models/Gif.js
+++ app/Models/Gif.js
@@ -1,9 +1,17 @@
 export default class Gif {
     constructor(data) {
         this.title = data.title
+        this.url = data.url || data.data.embed_url
     }
 
     get Template() {
-        return this.title
+        return `
+            <div class="card">
+                <img src="${this.url}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${this.title}</h5>
+                </div>
+            </div>
+        `
     }
 }
